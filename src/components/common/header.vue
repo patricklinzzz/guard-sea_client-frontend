2bda0514399c637204c785b2af3f4184688c4c8b
patricklinzzz <p9011910pat@gmail.com>
2025-07-18 22:09:31 +0800

    header、footer 漢堡還沒做

--- src/components/common/header.vue
+++ src/components/common/header.vue
@@ -4,44 +4,94 @@ import { RouterLink } from 'vue-router'
 
 <template>
     <header>
-        <div>
-            <router-link to="/"><img src="../../assets/images/logo.png" alt=""></router-link>
-            <nav>
+        <div id="navbar">
+            <router-link to="/"><img src="../../assets/images/logo.png" alt="" id="logo"></router-link>
+            <nav id="desktop_nav">
                 <router-link to="/">教育</router-link>
                 <router-link to="/">商品</router-link>
                 <router-link to="/">活動</router-link>
                 <router-link to="/">最新消息</router-link>
                 <router-link to="/">關於我們</router-link>
-                <router-link to="/"><img src="../../assets/images/icon_memder.svg" alt=""></router-link>
-                <router-link to="/"><img src="../../assets/images/icon_shopping.svg" alt=""></router-link>
+                <router-link to="/"><img src="../../assets/images/header/icon_memder.svg" alt=""
+                        width="40"></router-link>
+                <router-link to="/"><img src="../../assets/images/header/icon_shopping.svg" alt=""
+                        width="34"></router-link>
+            </nav>
+            <nav id="mobile_nav">
+                <router-link to="/"><img src="../../assets/images/header/icon_memder.svg" alt=""
+                        width="40"></router-link>
+                <router-link to="/"><img src="../../assets/images/header/icon_shopping.svg" alt=""
+                        width="34"></router-link>
+                <div id="hamburger">
+                    <img src="../../assets/images/header/icon_hamburger.svg" alt="" width="40">
+                </div>
             </nav>
         </div>
     </header>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 header {
-    background-color: #303030;
+    background-color: v.$color-blue-dark;
 
-    div {
+    #navbar {
         max-width: 1200px;
         height: 85px;
         display: flex;
         justify-content: space-between;
         align-items: center;
         margin: auto;
+        padding-left: 30px;
+        padding-right: 30px;
 
-        img {
-            height: 40px;
+        @include respond(md) {
+            padding-left: 15px;
+            padding-right: 15px;
         }
 
-        nav {
+        img {
+            display: flex;
+            justify-content: center;
+
+            @include respond(md) {
+                width: 30px;
+            }
+        }
+
+        #logo {
+            width: 100%;
+            height: 40px;
+            display: flex;
+            justify-content: center;
+        }
+
+        #desktop_nav {
             display: flex;
             align-items: center;
             gap: 30px;
 
+            @include respond(md) {
+                display: none;
+            }
+
             a {
                 color: #fff;
+                text-decoration-line: none;
+            }
+        }
+
+        #mobile_nav {
+            display: none;
+            align-items: center;
+            gap: 10px;
+
+            @include respond(md) {
+                display: flex;
+            }
+
+            a {
+                color: #fff;
+                text-decoration-line: none;
             }
         }
 
