ee04b73ece56ec3b88419c44ac79c1fd75c1397e
Syuan1996 <js7912288@gmail.com>
2025-07-21 17:49:15 +0800

    Add page NewListView、NewInsideView、AboutView、assets/images/new

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,10 @@ import SpeciesView from '@/views/Educate/SpeciesView.vue'
 import CausesView from '@/views/Educate/ExtinctionCausesView.vue'
 import QuizView from '@/views/Educate/QuizView.vue'
 import PorductListView from '@/views/Product/PorductListView.vue'
+import NewListView from '@/views/New/NewListView.vue'
+import NewInsideView from '@/views/New/NewInsideView.vue'
+import AboutView from '@/views/About/AboutView.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -37,6 +41,25 @@ const router = createRouter({
       name: 'ProductList',
       component: PorductListView,
     },
+      {
+      path: '/new',
+      name: 'newlist',
+      component: NewListView,
+    },
+       {
+      path: '/new/:id',
+      name: 'newinside',
+      component: NewInsideView,
+      props: true,
+    },
+    ,
+       {
+      path: '/about',
+      name: 'about',
+      component: AboutView,
+      
+    },
+    
 
     // {
     //   path: '/about',
