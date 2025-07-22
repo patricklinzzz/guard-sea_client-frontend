4794dca433a2d398ae36978ca75483aebe5df540
patricklinzzz <p9011910pat@gmail.com>
2025-07-23 00:45:28 +0800

    feat-按鈕範例(寫在首頁)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,21 @@
 <script setup>
-import Button from '@/components/buttons/button.vue'; 
+import Button from '@/components/buttons/button.vue';
+import { ref } from 'vue';
+
+const message = ref('456');
+const say123 = () => {
+  message.value = '123'
+}
+const say456 = () => {
+  message.value = '456'
+}
 </script>
 
 <template>
   <main>
-    <Button>確定報名</Button>
+    <Button @click="say123">確定報名</Button>
 
-    <Button variant="round"><i class="fa-solid fa-chevron-right"></i></Button>
+    <Button variant="round" @click="say456"><i class="fa-solid fa-chevron-right"></i></Button>
+    <p>{{ message }}</p>
   </main>
 </template>
