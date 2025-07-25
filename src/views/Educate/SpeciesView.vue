41d57bb1812d635de3f0939c70f2710bae392b07
Wen-31d067 <antonia31d067@gmail.com>
2025-07-25 15:36:37 +0800

    新增生物圖片及資料夾、修改生物頁及新增生物詳細頁

--- src/views/Educate/SpeciesView.vue
+++ src/views/Educate/SpeciesView.vue
@@ -1,168 +1,183 @@
 <script setup>
-import { ref, computed } from 'vue';
+  import { ref, computed } from 'vue'
 
-// 海域分類
-const areas = ['全部生物', '廣泛海域', '印度太平洋', '北太平洋', '極地海域', '特殊海域'];
-const currentArea = ref('全部生物');
+  // 海域分類
+  const areas = ['全部生物', '廣泛海域', '印度太平洋', '北太平洋', '極地海域', '特殊海域']
+  const currentArea = ref('全部生物')
 
-// 生物資料
-const animals = [
-  {
-    id: 1,
-    name: '綠蠵龜',
-    english: 'Chelonia mydas',
-    img: new URL('@/assets/images/Educate/General_Oceanic_Region/Chelonia_mydas.png', import.meta.url).href,
-    area: '廣泛海域',
-  },
-  {
-    id: 2,
-    name: '海獺',
-    english: 'Enhydra lutris',
-    img: new URL('@/assets/images/Educate/North_Pacific_Region/Enhydra_lutris.png', import.meta.url).href,
-    area: '北太平洋',
-  },
-  {
-    id: 3,
-    name: '皇帝企鵝',
-    english: 'Aptenodytes_forsteri',
-    img: new URL('@/assets/images/Educate/Polar_Region/Aptenodytes_forsteri.png', import.meta.url).href,
-    area: '極地海域',
-  },
-  {
-    id: 4,
-    name: '隆頭鸚哥魚',
-    english: 'Bolbometopon_muricatum',
-    img: new URL('@/assets/images/Educate/Indo_Pacific_Region/Bolbometopon_muricatum.png', import.meta.url).href,
-    area: '印度太平洋',
-  },
-  {
-    id: 5,
-    name: '鋸鰩',
-    english: 'ristis pristis',
-    img: new URL('@/assets/images/Educate/Special_Region/Pristis_pristis.png', import.meta.url).href,
-    area: '特殊海域',
-  },
-];
+  // 生物資料
+  const animals = [
+    {
+      id: 1,
+      name: '綠蠵龜',
+      english: 'Chelonia mydas',
+      img: new URL(
+        '@/assets/images/Educate/General_Oceanic_Region/Chelonia_mydas/Chelonia_mydas.png',
+        import.meta.url
+      ).href,
+      area: '廣泛海域',
+    },
+    {
+      id: 2,
+      name: '海獺',
+      english: 'Enhydra lutris',
+      img: new URL(
+        '@/assets/images/Educate/North_Pacific_Region/Enhydra_lutris/Enhydra_lutris.png',
+        import.meta.url
+      ).href,
+      area: '北太平洋',
+    },
+    {
+      id: 3,
+      name: '皇帝企鵝',
+      english: 'Aptenodytes_forsteri',
+      img: new URL('@/assets/images/Educate/Polar_Region/Aptenodytes_forsteri.png', import.meta.url)
+        .href,
+      area: '極地海域',
+    },
+    {
+      id: 4,
+      name: '隆頭鸚哥魚',
+      english: 'Bolbometopon_muricatum',
+      img: new URL(
+        '@/assets/images/Educate/Indo_Pacific_Region/Bolbometopon_muricatum.png',
+        import.meta.url
+      ).href,
+      area: '印度太平洋',
+    },
+    {
+      id: 5,
+      name: '鋸鰩',
+      english: 'ristis pristis',
+      img: new URL('@/assets/images/Educate/Special_Region/Pristis_pristis.png', import.meta.url)
+        .href,
+      area: '特殊海域',
+    },
+  ]
 
-const filteredAnimals = computed(() => {
-  if (currentArea.value === '全部生物') return animals;
-  return animals.filter((item) => item.area === currentArea.value);
-});
+  const filteredAnimals = computed(() => {
+    if (currentArea.value === '全部生物') return animals
+    return animals.filter((item) => item.area === currentArea.value)
+  })
 
-function changeArea(area) {
-  currentArea.value = area;
-}
+  function changeArea(area) {
+    currentArea.value = area
+  }
 </script>
 
 <template>
   <main class="wrapper">
     <div class="container">
-    <nav class="area-nav">
-      <button
-        v-for="area in areas"
-        :key="area"
-        :class="['area-btn', { active: currentArea === area }]"
-        @click="changeArea(area)"
-      >
-        <h3>{{ area }}</h3>
-      </button>
-    </nav>
+      <nav class="area-nav">
+        <button
+          v-for="area in areas"
+          :key="area"
+          :class="['area-btn', { active: currentArea === area }]"
+          @click="changeArea(area)"
+        >
+          <h3>{{ area }}</h3>
+        </button>
+      </nav>
 
-    <h2>{{ currentArea }}</h2>
+      <h2>{{ currentArea }}</h2>
       <section class="gallery">
-        <div
+        <router-link
           v-for="item in filteredAnimals"
           :key="item.id"
+          :to="`/edu/Species/1`"
           class="gallery__card"
         >
-          <img
-            :src="item.img"
-            :alt="item.name"
-            class="gallery__img"
-          />
+          <img :src="item.img" :alt="item.name" class="gallery__img" />
           <h3>{{ item.name }}</h3>
-        <p class="sub-text"><em>{{ item.english }}</em></p>
-        </div>
+          <p class="sub-text">
+            <em>{{ item.english }}</em>
+          </p>
+        </router-link>
       </section>
     </div>
   </main>
 </template>
 
 <style lang="scss" scoped>
-.wrapper::before{
-@include bg-layer-fixed-dark;
-}
+  .wrapper::before {
+    @include bg-layer-fixed-dark;
+  }
 
-main {
-  background-color: v.$color-blue-dark;
-  min-height: 100vh;
-  // margin: 0 auto;
-  // height: 100%;
-  .container {
-    width: 100%;
-    margin: 0 auto;
-    padding: 0 16px;
-    max-width: 1200px;
-    .area-nav{
-    display: flex;
-    justify-content: center;
-    gap: 12px;
-    margin: 16px auto;
+  main {
+    background-color: v.$color-blue-dark;
+    min-height: 100vh;
+    // margin: 0 auto;
+    // height: 100%;
+    .container {
+      width: 100%;
+      margin: 0 auto;
+      padding: 0 16px;
+      max-width: 1200px;
+      .area-nav {
+        display: flex;
+        justify-content: center;
+        gap: 12px;
+        margin: 16px auto;
+      }
+      .area-btn {
+        all: unset;
+        background-color: transparent;
+        color: v.$color-white;
+        border-radius: 6px;
+        cursor: pointer;
+        transition: background-color 0.3s ease;
+        outline: none;
+        box-shadow: none;
+        padding: 3px 4px;
+
+        &:hover {
+          background-color: v.$color-skyblue;
+        }
+        //   &:active {
+        //   background-color:v.$color-skyblue;
+        // }
+        //   }
+      }
+      .gallery {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr); // 手機：2欄
+        gap: 5px;
+        text-align: center;
+        .gallery__card {
+          text-decoration: none;
+          color: inherit;
+          display: block;
+
+          &:hover {
+            color: inherit;
+          }
+        }
+
+        @media (min-width: v.$breakpoint-md) {
+          grid-template-columns: repeat(4, 1fr); // 桌面：4欄
+        }
+      }
     }
-  .area-btn {
-    all: unset;
-    background-color: transparent;
+  }
+
+  h2,
+  h3,
+  .sub-text {
     color: v.$color-white;
-    border-radius: 6px;
-    cursor: pointer;
-    transition: background-color 0.3s ease;
-    outline: none;
-    box-shadow: none;
-    padding: 3px 4px;
-
-  &:hover {
-    background-color:v.$color-skyblue;
-  }
-  //   &:active {
-  //   background-color:v.$color-skyblue;
-  // }
-  //   }
-}
-    .gallery {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr); // 手機：2欄
-  gap: 5px;
-  text-align: center;
-
-  @media (min-width: v.$breakpoint-md) {
-    grid-template-columns: repeat(4, 1fr); // 桌面：4欄
-  }
-}
-  }
   }
 
+  h2 {
+    margin: 0 auto;
+    text-align: center;
+  }
 
-h2,
-h3,
-.sub-text {
-  color: v.$color-white;
-}
+  .sub-text {
+    font-style: italic;
+  }
 
-h2 {
-  margin: 0 auto;
-  text-align: center;
-}
-
-.sub-text {
-  font-style: italic;
-}
-
-.gallery__img {
-  width: 100%;
-  height: auto;
-  object-fit: cover;
-}
-
-
-
+  .gallery__img {
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+  }
 </style>
---
+++ src/views/Educate/SpeciesinfoView.vue
@@ -0,0 +1,160 @@
+<script setup>
+  import { ref, onMounted } from 'vue'
+  const props = defineProps(['id'])
+  const animal = ref({})
+  // animal.value = animals[id]
+  const animals = [
+    {
+      id: 1,
+      areas: '廣泛海域',
+      name: '綠蠵龜',
+      english: 'Chelonia mydas',
+      status: 'CR極危',
+      classification: '爬蟲綱／海龜科',
+      length: '可達 1.5 公尺',
+      weight: '最多超過 200 公斤',
+      lifespan: '可活超過 80 年',
+      distribution: '全球熱帶與亞熱帶海域，包括台灣東部、蘭嶼、小琉球一帶',
+      image1: new URL(
+        '../../assets/images/Educate/General_Oceanic_Region/Chelonia_mydas1.jpg',
+        import.meta.url
+      ).href,
+      image2: new URL(
+        '../../assets/images/Educate/General_Oceanic_Region/Chelonia_mydas2.jpg',
+        import.meta.url
+      ).href,
+      image3: new URL(
+        '../../assets/images/Educate/General_Oceanic_Region/Chelonia_mydas2.jpg',
+        import.meta.url
+      ).href,
+      description:
+        '綠蠵龜是體型最大的草食性海龜，廣泛分布於熱帶與亞熱帶海域，包括台灣東部與小琉球。牠們以海草和藻類為主食，具有強烈的歸巢性，會跨越數千公里回到出生地產卵。 目前綠蠵龜被國際自然保育聯盟列為瀕危物種，主要受到漁業混獲、海岸開發、垃圾污染與非法獵捕等威脅。保育行動包含參與淨灘、避免購買龜製品與推廣海洋教育，讓更多人關注牠們的生存困境與生態價值。',
+    },
+  ]
+  onMounted(() => {
+    animal.value = animals.find((a) => a.id === Number(props.id)) || animals[0]
+  })
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="container">
+      <div class="images" v-if="animal.image1">
+        <img :src="animal.image1" alt="圖片 1" />
+      </div>
+      <!-- <p class="english">{{ animals[0].english }}</p> -->
+
+      <div class="animalInfo">
+        <h1 class="areas">{{ animal.areas }}</h1>
+        <h2 class="name">{{ animal.name }}</h2>
+        <ul>
+          <li>
+            <p>學名：{{ animal.english }}</p>
+          </li>
+          <li>
+            <p>分類：{{ animal.classification }}</p>
+          </li>
+          <li>
+            <p>體長：{{ animal.length }}</p>
+          </li>
+          <li>
+            <p>瀕危指標：{{ animal.status }}</p>
+          </li>
+          <li>
+            <p>體重：{{ animal.weight }}</p>
+          </li>
+          <li>
+            <p>壽命：{{ animal.lifespan }}</p>
+          </li>
+          <li>
+            <p>分布：{{ animal.distribution }}</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="description">
+    <div class="description_first">
+      <h2>詳細介紹</h2>
+      <p>{{ animal.description }}</p>
+    </div>
+
+    <div class="images" v-if="animal.image1">
+      <img :src="animal.image2" alt="圖片 2" />
+      <img :src="animal.image2" alt="圖片 3" />
+    </div>
+  </div>
+
+  <div class="viewMore"><h1>點擊生物看更多</h1></div>
+  <!-- <div class="animals"></div> -->
+</template>
+
+<style lang="scss" scoped>
+  .wrapper {
+    background-color: v.$color-blue-dark;
+  }
+  .container {
+    width: 90%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    // gap: 10px; // 區塊之間留間距
+    color: v.$color-text-white;
+    padding: 30px 0px;
+  }
+
+  .images {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    align-items: center;
+
+    img {
+      width: 100%;
+      aspect-ratio: 4 / 3;
+      object-fit: cover;
+    }
+  }
+  .animalInfo {
+    padding: 10px 0px;
+  }
+
+  .description {
+    max-width: 90%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    margin-top: 20px;
+    text-align: center;
+    padding-bottom: 15px;
+  }
+  .description_first {
+    padding-bottom: 20px;
+    // gap: 10px;
+  }
+  .viewMore {
+    margin: 0 auto;
+    text-align: center;
+  }
+  @media (min-width: 768px) {
+    .container {
+      display: flex;
+      flex-direction: row;
+      gap: 30px;
+    }
+
+    .animalInfo {
+      display: flex;
+      flex-direction: column;
+    }
+    .images {
+      display: flex;
+      width: calc(50%);
+      flex-direction: row;
+    }
+    .description img {
+      width: 100%;
+    }
+  }
+</style>
