7f3e7c8c4ea2c6183929b8ee898c99b48912b236
patricklinzzz <p9011910pat@gmail.com>
2025-07-28 20:50:18 +0800

    部屬

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,52 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig ,loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    plugins: [vue(), vueDevTools()],
+    base: env.VITE_BASE || '/',
+    build: { outDir: env.VITE_OUT_DIR || 'dist' },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
           @use '@/assets/style/mixins' as *;
           @use '@/assets/style/variables' as v;
-        `
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+        `,
+        },
+      },
     },
-  },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  }
 })
+
+// https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//     vueDevTools(),
+//   ],
+//   css: {
+//     preprocessorOptions: {
+//       scss: {
+//         additionalData: `
+//           @use '@/assets/style/mixins' as *;
+//           @use '@/assets/style/variables' as v;
+//         `
+//       }
+//     }
+//   },
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     },
+//   },
+// })
