dc3360387e89691a2ea76e6330097ecaa6255e86
Ben_Lin <benlin96186@gmail.com>
2025-07-30 13:43:06 +0800

    feat- quiz logic first draft(missing main nav, RWD, background img)

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <script setup>
-  import { RouterView } from 'vue-router'
+  import { RouterView, useRoute } from 'vue-router'
   import Header from '@/components/common/header.vue'
   import Footer from '@/components/common/footer.vue'
+
+  import { ref, computed, provide } from 'vue'
+
+  //重新測驗頁面邏輯
+  const route = useRoute()
+  const quizPageKey = ref(0)
+  const calculatedRouterViewKey = computed(() => {
+    if (route.name === 'quiz') {
+      return quizPageKey.value
+    }
+    return route.fullPath
+  })
+
+
+  const forceQuizRefresh = () => {
+    quizPageKey.value++ 
+  }
+
+  provide('forceQuizRefresh', forceQuizRefresh)
 </script>
 
 <template>
   <Header></Header>
-  <RouterView />
+  <RouterView :key="calculatedRouterViewKey" />
   <Footer></Footer>
 </template>
 
