2edb6a01dd98ceab89461a49226e65d5f70a1580
Syuan1996 <js7912288@gmail.com>
2025-08-01 01:33:22 +0800

    renew newlist and about-newwave

--- src/views/About/AboutView.vue
+++ src/views/About/AboutView.vue
@@ -1,5 +1,10 @@
 <script setup>
   import aboutwhiteblock from '@/assets/images/about/aboutwhiteblock.png'
+  import feel from '@/assets/images/about/advantage/feel.jpg'
+  import know from '@/assets/images/about/advantage/know.jpg'
+  import protect from '@/assets/images/about/advantage/protect.jpg'
+  import diver from '@/assets/images/about/diver.png'
+  import seaturtle from '@/assets/images/about/seaturtle.png'
 </script>
 
 <template>
@@ -19,21 +24,43 @@
 
           <p>-- 這個平台因此誕生</p>
         </div>
-      </div>
-      <div class="wave_block">
-        <div class="wave"></div>
+        <div class="diver">
+          <img :src="diver" alt="" />
+        </div>
+        <div class="seaturtle">
+          <img :src="seaturtle" alt="" />
+        </div>
       </div>
     </section>
 
-    <div></div>
-
     <section id="about_detail">
-      <p>我們致力於打造一個 讓人們能夠感受海洋、認識海洋、守護海洋的數位行動基地</p>
+      <div class="wave_block">
+        <div class="wave"></div>
+      </div>
 
-      <p>讓更多人了解海洋的生命，願意一起守護牠們的家</p>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div class="idea">
+        <h2>我們致力於打造一個</h2>
+
+        <h2>讓人們能夠感受海洋、認識海洋、守護海洋的數位行動基地</h2>
+      </div>
+
+      <div class="advantage">
+        <div>
+          <img :src="feel" alt="" />
+          <h2>感受</h2>
+        </div>
+        <div>
+          <img :src="know" alt="" />
+          <h2>認識</h2>
+        </div>
+        <div>
+          <img :src="protect" alt="" />
+          <h2>守護</h2>
+        </div>
+      </div>
+
+      <div class="dolphin"></div>
+      <div class="whale"></div>
     </section>
   </main>
 </template>
@@ -51,12 +78,36 @@
   #banner {
     position: relative;
     width: 100%;
-    height: 1200px;
+
     background-image: url('@/assets/images/about/aboutbanner.jpg');
     background-repeat: no-repeat;
     background-size: cover;
+    padding-bottom: 8.5vw;
 
-    background-position: center -210px;
+    @media screen and (max-width: 1800px) {
+      padding-bottom: 10vw;
+    }
+
+    @media screen and (max-width: 1500px) {
+      padding-bottom: 13vw;
+    }
+
+    @media screen and (max-width: 1200px) {
+      padding-bottom: 15vw;
+    }
+
+    @include respond(md) {
+      padding-bottom: 30vw;
+    }
+
+    @media screen and (max-width: 420px) {
+      padding-bottom: 35vw;
+    }
+
+    @media screen and (max-width: 350px) {
+      padding-bottom: 38vw;
+    }
+
     h1 {
       text-align: center;
       padding: 5.8333333% 20px 0px 20px;
@@ -122,66 +173,160 @@
         }
       }
     }
-  }
 
-  .wave_block {
-    position: absolute;
-    width: 100%;
-    height: 280px;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    overflow: hidden;
+    // diver_start
+
+    .diver {
+      position: absolute;
+      z-index: 1;
+      left: -10%;
+      top: 65%;
+      @include respond(md) {
+        transform: scale(0.75);
+        left: -15%;
+        top: 78%;
+      }
+
+      @media screen and (max-width: 480px) {
+        left: -29%;
+
+        top: 70%;
+        transform: scale(0.7) rotate(-40deg);
+      }
+
+      @media screen and (max-width: 380px) {
+        left: -10%;
+        top: 78%;
+        transform: scale(0.6);
+      }
+    }
+    .seaturtle {
+      position: absolute;
+      z-index: 1;
+      right: 3%;
+      @include respond(md) {
+        transform: scale(0.75);
+      }
+      @media screen and (max-width: 480px) {
+        transform: scale(0.7);
+        top: 78%;
+      }
+
+      @media screen and (max-width: 380px) {
+        right: 50%;
+        top: 65%;
+        transform: scale(0.6);
+      }
+    }
+    // diver_end
   }
 
   // banner_end
 
-  .wave {
-    background-image: url('/src/assets/images/about/aboutwave.png');
-    width: 100%;
-    height: 100%;
-
-    animation: wave 12s cubic-bezier(0.36, 0.45, 0.63, 0.53) infinite;
-
-    background-size: 1920px 100%;
-    background-position-x: 0;
-  }
-
-  @keyframes wave {
-    0% {
-      background-position-x: 0;
-    }
-    100% {
-      background-position-x: -1920px;
-    }
-  }
-
-  @include respond(md) {
-    .wave_block {
-      height: 300px;
-    }
-    .wave {
-      background-size: 1000px auto;
-      animation: waveMobile 12s linear infinite;
-    }
-
-    @keyframes waveMobile {
-      0% {
-        background-position-x: 0px;
-      }
-      100% {
-        background-position-x: -1000px;
-      }
-    }
-  }
-
-  // about_detail_start
+  // // about_detail_start
 
   #about_detail {
-    @include respond(md) {
-      position: relative;
-      top: -160px;
+    position: relative;
+
+    //wave_start
+
+    .wave_block {
+      position: absolute;
+      width: 100%;
+      height: 85px;
+      top: -84px;
+      overflow: hidden;
+      // transform: rotate(180deg); // 如果波浪是白色往上疊就轉一下
     }
+    .wave {
+      //背景圖大小2100px x 85px高
+      background-image: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyMTAwIiBoZWlnaHQ9Ijg1IiBmaWxsPSJub25lIj48cGF0aCBmaWxsPSIjMDEyMDM4IiBkPSJNMTQwMCA3Ni44NzhjLTEzNi4wNiAwLTE3OS4yOS03Ni45NzUtMzU0LjgxLTc2Ljg3OEM4NzAuMTg4IDAgODI2LjQzOCA3Ni44NzggNzAwIDc2Ljg3OFY4NWg3MDB2LTguMTIyWk03MDAgNzYuODc4QzU2My45MzggNzYuODc4IDUyMC43MTMtLjA5NyAzNDUuMTg4IDAgMTcwLjE4OCAwIDEyNi40MzggNzYuODc4IDAgNzYuODc4Vjg1aDcwMHYtOC4xMjJaTTIxMDAgNzYuODc4Yy0xMzYuMDYgMC0xNzkuMjktNzYuOTc1LTM1NC44MS03Ni44NzgtMTc1IDAtMjE4Ljc1IDc2Ljg3OC0zNDUuMTkgNzYuODc4Vjg1aDcwMHYtOC4xMjJaIi8+PC9zdmc+');
+      display: block;
+
+      width: 6300px;
+      height: 100%;
+      background-repeat: repeat-x;
+      background-size: 2100px 100%;
+      animation: wave 4s cubic-bezier(0.36, 0.45, 0.63, 0.53) infinite;
+
+      transform: translateX(0);
+    }
+    @keyframes wave {
+      0% {
+        transform: translateX(0);
+      }
+      100% {
+        transform: translateX(-2100px);
+      }
+    }
+
+    //wave_end
+    .idea {
+      padding-top: 6%;
+      margin-bottom: 4vw;
+
+      @include respond(md) {
+        padding-top: 9%;
+      }
+
+      @media screen and (max-width: 480px) {
+        padding-top: 14%;
+      }
+
+      h2 {
+        padding: 0 10px;
+        text-align: center;
+        margin-bottom: 15px;
+        &:last-child {
+          @include respond(md) {
+            padding: 0 20px;
+            padding-top: 6%;
+            line-height: 1.6;
+          }
+        }
+      }
+    }
+
+    //advantage_start
+
+    .advantage {
+      max-width: 980px;
+      padding: 0 20px;
+      display: flex;
+      justify-content: space-between;
+      gap: 5%;
+      margin: 0 auto;
+
+      @include respond(md) {
+        flex-wrap: wrap;
+        justify-content: center;
+      }
+      div {
+        position: relative;
+        width: 31.25%;
+        aspect-ratio: 1 / 1;
+        border-radius: 50%;
+        overflow: hidden;
+
+        @include respond(md) {
+          width: 45%;
+        }
+        img {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+        }
+        h2 {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          text-shadow: 0px 2px 6px rgba(0, 0, 0, 0.8);
+        }
+      }
+    }
+
+    //advantage_end
   }
 
   // about_detail_end
